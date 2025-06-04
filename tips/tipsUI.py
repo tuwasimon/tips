f00476b61573de6859078facf5e396fced98cd2b
tuwasimon <tuwa1076@gmail.com>
2025-06-04 08:06:05 +0100

    Add files via upload

---
+++ tips/tipsUI.py
@@ -0,0 +1,19 @@
+import joblib
+import streamlit as st
+import numpy as np
+
+
+model = joblib.load('tips.pkl')
+
+total_bill = st.number_input('Enter total bill')
+tip = st.number_input('Enter tip')
+size = st.number_input('enter size')
+
+if st.button('Classify'):
+    input_features = np.array([[total_bill, tip, size]])
+    pred = model.predict(input_features)
+    mapping = ['Male', 'Female']
+    st.success(f'The prediction is {pred}{mapping}[0]') 
+
+
+
